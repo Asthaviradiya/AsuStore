5301d9173df942b9b29091729783d7a50aa0daed
Asthaviradiya <asthaviradiya114@gmail.com>
2024-06-03 11:52:34 +0530

    removeProdFromCart js file is created and updated

--- e-store/fetchQuantityFromCartLS.js
+++ e-store/fetchQuantityFromCartLS.js
@@ -15,4 +15,4 @@ export const fetchQuantityFromCartLS = (id, price) => {
   return { quantity, price };
 };
 
-//todo Don't Forget To LIKE SHARE & SUBSCRIBE TO THAPA TECHNCIAL YOUTUBE CHANNEL 👉 https://www.youtube.com/thapatechnical
+
